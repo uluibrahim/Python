c47d0a76f03bef42389ce472f174242a218b65eb
uluibrahim <62259512+uluibrahim@users.noreply.github.com>
2020-04-27 22:52:30 +0300

    Add files via upload

---
+++ veri_tip_donusumleri.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 15:36:30 2020
+
+@author: ibrah
+"""
+# *** VERİ TİPİ DÖNÜŞÜMLERİ ***
+""" 
+# NOT: kullanıcıdan deger almak için input kullanılır.
+# inputlar string olarak deger alır
+
+sayi1=input("sayi1: ")
+sayi2=input("sayi2: ")
+
+print(type(sayi1))
+print(type(sayi2))
+
+# string olan sayi1 ve sayi2 değişkenlerini int tipine dönüştürdük.
+sayi1=int(sayi1)
+sayi2=int(sayi2) 
+
+toplam=sayi1+sayi2
+print(toplam)
+
+"""
+
+x=5
+y=2.5
+name="ibrahim"
+isOnline=True
+
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(isOnline))
+
+# x değişkeni int iken float a dönüştürüldü
+x=float(x)
+print(x)
+print(type(x))
+
+# y değişkeni float iken int e dönüştürüldü
+y=int(y)
+print(y)
+print(type(y))
+
+# isOnline değişkeni bool iken int e dönüştürüldü
+isOnline=int(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+# isOnline değişkeni int iken str e dönüştürüldü
+isOnline=str(isOnline)
+print(isOnline)
+print(type(isOnline))
+
